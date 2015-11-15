eecce90575cd108bd97eb1bee125d7f3d0e96145
JiaChunyang <jiachunyang@JiaChunyangdeMacBook-Pro.local>
2015-11-15 19:57:38 +0800

    the second day with orm model

--- awesome-python3-webapp/www/ing.py
+++ awesome-python3-webapp/www/ing.py
@@ -84,5 +84,63 @@ class Model(dict,metaclass=ModelMetaclass):
                 setattr(self,key,value)
         return value
         
+#定义Filed and Filed的子类#
+class Field(object):
+    def __init__(self,name,column_type,primary_key,default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+        
+    def __str__(self):
+        return '<%s,%s:%s>' % (self.__clasee__.__name__,self.column_type,self.name)
+        
+class StringField(Field):
+    
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+        super().__init__(name,ddl,primary_key,default)
+        
+class ModelMetaclass(type):
+    
+    def __new__(cls,name,bases,attrs):
+        #排除Model类本身
+        if name=='Model':
+            return type.__new__(cls,name,bases,attrs)
+        #获取table名称：
+        tableName = attrs.get('__table__',None) or name
+        logging.info('found model:%s (table:%s)'%(name,tableName))
+        #获取所有的Field和主键名：
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k,v in attrs.items():
+            if isinstance(v,Field):
+                logging.info(' found mpping:%s ==>%s'%(k,v))
+                mapping[k] = v
+                if v.primary_key:
+                    #找到主键:
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field:%s' %k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('primary key not found.')
+        for k in mapping.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mapings__'] = mapping #保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey #主键属性名
+        attrs['__fields__'] = fields #除主键外的属性名
+        #构造默认的select,insert,update和DELETE语句：
+        attrs[__'select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+        
+ #这样，任何继承自Model的类（比如user），会自动通过ModelMtaclass扫描映射关系，并存储到自身的雷属性如__table__/__mappings__中
+    
             
     
